536526ab14633708aaa265fddf9c9c6a780e2a3f
Swarup Ukil <77638087+SwarupUkil@users.noreply.github.com>
2023-07-12 02:19:48 -0400

    Implement the main game() function
    All the functionality to the game is implemented. The player can choose
    either rock, paper, or scissors and battle the computer who will
    randomly choose either rock, paper, or scissors. The player is continually
    prompted until they complete the 5 rounds. It ends with either the player
    winning, losing, or drawing.

--- script.js
+++ script.js
@@ -16,17 +16,42 @@ function playRound(playerSelection="rock", computerSelection){
 
     playerSelection = playerSelection.toLowerCase();
 
-    if(playerSelection === computerSelection){
-        return `Draw! ${playerSelection} ties ${computerSelection}`;
+    if (playerSelection === computerSelection){
+        return [`Draw! ${playerSelection} ties ${computerSelection}`, 1, 1];
     }else if ((playerSelection === "rock" && computerSelection === "paper") ||
               (playerSelection === "paper" && computerSelection === "scissors") ||
               (playerSelection === "scissors" && computerSelection === "rock")){
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        return [`You Lose! ${computerSelection} beats ${playerSelection}`, 0, 1];
     }else if ((playerSelection === "rock" && computerSelection === "scissors") ||
               (playerSelection === "paper" && computerSelection === "rock") ||
               (playerSelection === "scissors" && computerSelection === "paper")){
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
+        return [`You Win! ${playerSelection} beats ${computerSelection}`, 1, 0];
     }else{
-        return "Bad input, try again."
+        return ["Bad input, try again.", 0, 0]
+    }
+}
+
+function game(){
+    let playerTotalPoints, computerTotalPoints;
+    playerTotalPoints = computerTotalPoints = 0;
+
+    for (let i=0; i < 5; i++){
+        playerSelection = prompt("Input your choice: Rock, Paper, or Scissors.");
+        let result = playRound(playerSelection, getComputerChoice());
+        console.log(result[0]);
+        playerTotalPoints += result[1];
+        computerTotalPoints += result[2];
+
+        if (result[0] === "Bad input, try again."){
+            i--;
+        }
+    }
+
+    if (playerTotalPoints > computerTotalPoints){
+        console.log("You Win!")
+    }else if (playerTotalPoints < computerTotalPoints){
+        console.log("You Lose.")
+    }else{
+        console.log("You Tied.")
     }
 }
