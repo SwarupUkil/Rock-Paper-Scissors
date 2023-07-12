34171df1ccc285b83fb727e98606af78bcec6d03
Swarup Ukil <77638087+SwarupUkil@users.noreply.github.com>
2023-07-12 02:02:29 -0400

    Implement playRound function;plays 1 round of game

--- script.js
+++ script.js
@@ -4,10 +4,29 @@ function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 10000);
     switch(randomNumber % 3){
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors";
+            return "scissors";
+    }
+}
+
+function playRound(playerSelection="rock", computerSelection){
+
+    playerSelection = playerSelection.toLowerCase();
+
+    if(playerSelection === computerSelection){
+        return `Draw! ${playerSelection} ties ${computerSelection}`;
+    }else if ((playerSelection === "rock" && computerSelection === "paper") ||
+              (playerSelection === "paper" && computerSelection === "scissors") ||
+              (playerSelection === "scissors" && computerSelection === "rock")){
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+              (playerSelection === "paper" && computerSelection === "rock") ||
+              (playerSelection === "scissors" && computerSelection === "paper")){
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }else{
+        return "Bad input, try again."
     }
 }
